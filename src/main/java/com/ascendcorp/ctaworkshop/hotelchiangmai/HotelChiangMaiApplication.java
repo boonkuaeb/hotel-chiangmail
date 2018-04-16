01d143aafb374da98740b5ad50fa649c9e5011a6
Thanakorn Thanprasit <thanakorn_thanprasit@ascendinter.com>
2018-04-16 23:57:50 +0700

    Change config of CORS

--- src/main/java/com/ascendcorp/ctaworkshop/hotelchiangmai/HotelChiangMaiApplication.java
+++ src/main/java/com/ascendcorp/ctaworkshop/hotelchiangmai/HotelChiangMaiApplication.java
@@ -6,29 +6,24 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class HotelChiangMaiApplication extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(HotelChiangMaiApplication.class);
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HotelChiangMaiApplication.class, args);
 	}
 
     @Bean
-    public WebMvcConfigurerAdapter corsConfigurer() {
+    public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("*").allowedOrigins("*").allowedMethods("POST, GET, OPTIONS, DELETE, PATCH, PUT")
-                        .allowedHeaders("X-Requested-With, Content-Type, Accept, Origin, Cache-Control, Authorization, X-WM-AnonymousId, X-WM-CorrelationId, X-Wm-AccessToken, X-Wm-RefreshToken, X-ACT-AccessToken, X-ACT-RefreshToken, X-ACT-ByPassToken")
-                        .allowCredentials(false).maxAge(3600);
+                registry.addMapping("*").allowedOrigins("*");
             }
         };
     }
+
 }
