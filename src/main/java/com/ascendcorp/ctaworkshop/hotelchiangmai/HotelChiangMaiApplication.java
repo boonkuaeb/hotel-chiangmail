3ef80fce1cd666db15ae39c5de3ab7aaa47d7c60
Thanakorn Thanprasit <thanakorn_thanprasit@ascendinter.com>
2018-04-16 22:26:35 +0700

    Add initial for war

--- src/main/java/com/ascendcorp/ctaworkshop/hotelchiangmai/HotelChiangMaiApplication.java
+++ src/main/java/com/ascendcorp/ctaworkshop/hotelchiangmai/HotelChiangMaiApplication.java
@@ -2,9 +2,16 @@ package com.ascendcorp.ctaworkshop.hotelchiangmai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HotelChiangMaiApplication {
+public class HotelChiangMaiApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HotelChiangMaiApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HotelChiangMaiApplication.class, args);
