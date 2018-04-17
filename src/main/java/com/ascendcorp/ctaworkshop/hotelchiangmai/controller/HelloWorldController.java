878ca4294fbb5461bb91b31352b23c62dd52804a
Thanakorn Thanprasit <thanakorn_thanprasit@ascendinter.com>
2018-04-18 01:29:41 +0700

    Update Test poll jenkins

--- src/main/java/com/ascendcorp/ctaworkshop/hotelchiangmai/controller/HelloWorldController.java
+++ src/main/java/com/ascendcorp/ctaworkshop/hotelchiangmai/controller/HelloWorldController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "Greetings from Spring Boot ja!";
     }
 }
